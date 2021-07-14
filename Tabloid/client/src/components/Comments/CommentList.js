0f30ea36cc6dcd25a356342f38452b4ee1a0dae8
bvinson23 <brandonvinson23@gmail.com>
2021-07-14 14:22:25 -0500

    completed list component

--- Tabloid/client/src/components/Comments/CommentList.js
+++ Tabloid/client/src/components/Comments/CommentList.js
@@ -1,3 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Comment from "./Comment";
-import { getAllComments } from "../modules/commentManager";
+import { getAllCommentsByPost } from "../modules/commentManager";
+
+const CommentList = () => {
+    const [comments, setComments] = useState([]);
+    
+    const getComments = () => {
+        getAllCommentsByPost().then(comments => setComments(comments));
+    };
+
+    useEffect(() => {
+        getComments();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {comments.map((comment) => (
+                    <Comment comment={comment} key={comment.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default CommentList;
