036d7c1c2f068412c26dfbe76e498f4309f6c470
Logan Demmy <frank.demmy@gmail.com>
2021-07-16 10:07:12 -0500

    rocketship

--- Tabloid/client/src/components/Login.js
+++ Tabloid/client/src/components/Login.js
@@ -3,7 +3,7 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useHistory, Link } from "react-router-dom";
 import { login } from "../modules/authManager";
 import "./Login.css";
-
+import rocket from "./rocketship.png"
 export default function Login() {
   const history = useHistory();
 
@@ -19,24 +19,27 @@ export default function Login() {
 
   return (
     <div className="container-login">
-    <Form onSubmit={loginSubmit}>
-      <fieldset>
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input id="email" type="text" autoFocus onChange={e => setEmail(e.target.value)} />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
-        </FormGroup>
-        <FormGroup>
-          <Button>Login</Button>
-        </FormGroup>
-        <em>
-          Not registered? <Link to="register">Register</Link>
-        </em>
-      </fieldset>
-    </Form>
+      <img src={rocket} alt="rocketship logo" 
+              
+               ></img>
+      <Form onSubmit={loginSubmit}>
+        <fieldset>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input id="email" type="text" autoFocus onChange={e => setEmail(e.target.value)} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
+          </FormGroup>
+          <FormGroup>
+            <Button>Login</Button>
+          </FormGroup>
+          <em>
+            Not registered? <Link to="register">Register</Link>
+          </em>
+        </fieldset>
+      </Form>
     </div>
   );
 }
