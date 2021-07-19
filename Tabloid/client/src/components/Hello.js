0614e39b73934ae7dd7f7a94881f6211128115e6
Logan Demmy <frank.demmy@gmail.com>
2021-07-19 11:07:38 -0500

    fixed subscription error

--- Tabloid/client/src/components/Hello.js
+++ Tabloid/client/src/components/Hello.js
@@ -1,6 +1,6 @@
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 import { getSubscribedPosts } from "../modules/subscriptManager.js";
-import  Post from "./posts/PostListCard.js"
+import Post from "./posts/PostListCard.js"
 import thing from "./TABLOIDBOLD.jpg"
 import "./Hello.css";
 
@@ -12,20 +12,21 @@ export default function Hello() {
     getSubscribedPosts().then(posts => setPosts(posts))
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserSubscription()
-  },[])
+  }, [])
 
   return (
     <div>
-    <img src={thing}  className="centerme" alt="user img"/>
-    <div>
-      <h2>Subscriptions</h2>
-        {posts.map((post) => {
-                            return <Post key={post.id} post={post} />;
-                        })}
-
-    </div>
+      <img src={thing} className="centerme" alt="user img" />
+      <div className="justify-content-center">
+        <h2>Subscriptions</h2>
+        <div className="subscriptions" style={{ backgroundImage: thing }}>
+          {posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
